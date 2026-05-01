26c99966a7df3acbb1261a638376fff13cfb47ad
Praseeba P <praseebap@gmail.com>
2026-05-01 09:50:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 data = []
 
-for i in range(10):
-    value = random.randint(20, 50)  # simulate sensor data
-    data.append(value)
-    
-    print("Sensor Value:", value)
-    time.sleep(1)
+plt.ion()
 
-plt.plot(data)
-plt.title("IoT Sensor Data")
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.show()
+for i in range(20):
+    value = random.randint(20, 50)
+    data.append(value)
+
+    plt.clf()
+    plt.plot(data)
+    plt.title("Real-Time IoT Data")
+    plt.pause(0.5)
+
+    print("Sensor:", value)
